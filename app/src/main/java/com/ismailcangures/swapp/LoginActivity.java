1dd78f4253fa035a3789ff1594bed09c3ab7ed6a
Can Gures <cangures1912@gmail.com>
2023-05-19 18:44:45 +0300

    Signup & Login Screens

--- app/src/main/java/com/ismailcangures/swapp/LoginActivity.java
+++ app/src/main/java/com/ismailcangures/swapp/LoginActivity.java
@@ -2,13 +2,30 @@ package com.ismailcangures.swapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+
     }
 }
