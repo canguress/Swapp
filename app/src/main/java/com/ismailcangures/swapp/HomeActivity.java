f19def69be302ce46517f9f2639ccb5899f3d599
Can <canguresl1912@gmail.com>
2023-05-20 17:15:46 +0300

    Features Added
    *Bottom Navigation Bar Design
    *HomeScreen Fragment
    *MessagesScreen Fragment
    **DiscoverScreen Fragment
    *AddProductScreen Fragment
    *ProfileScreen Fragment
    
    features has been added.

--- app/src/main/java/com/ismailcangures/swapp/HomeActivity.java
+++ app/src/main/java/com/ismailcangures/swapp/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.ismailcangures.swapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,25 +11,50 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+
+import com.ismailcangures.swapp.databinding.ActivityHomeBinding;
+import com.ismailcangures.swapp.databinding.ActivityMainBinding;
+import com.ismailcangures.swapp.fragments.HomeFragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
 public class HomeActivity extends AppCompatActivity {
 
-    Button btnLogOut;
+    ActivityHomeBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        btnLogOut = findViewById(R.id.btnLogOut);
+         binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        //binding.bottomNavigationView
 
-        btnLogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                Intent i = new Intent(HomeActivity.this,MainActivity.class);
-                Toast.makeText(HomeActivity.this,"You logged out..",Toast.LENGTH_SHORT).show();
-                startActivity(i);
-                finish();
-            }
-        });
+
+
+
+
+
+
+
+
+
+
     }
+
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+
+    }
+
+
 }
--- app/src/main/java/com/ismailcangures/swapp/MainActivity.java
+++ app/src/main/java/com/ismailcangures/swapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.ismailcangures.swapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btnLogin, btnSignUp;
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         category.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, CategoryListActivity.class);
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
                 startActivity(i);
                 finish();
             }
